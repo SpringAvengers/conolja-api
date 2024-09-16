df700b43a03aac86dc9d30d6fad53c50a58320cb
Jhung Minwoo <minwoorich96@gmail.com>
2024-09-16 21:45:56 +0900

    init-commit

---
+++ src/main/java/site/javaghost/conolja/ConoljaApplication.java
@@ -0,0 +1,13 @@
+package site.javaghost.conolja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConoljaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConoljaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/javaghost/conolja/ConoljaApplicationTests.java
@@ -0,0 +1,13 @@
+package site.javaghost.conolja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConoljaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
