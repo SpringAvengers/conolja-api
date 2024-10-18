12089a1a6b8e4223275c204a470e6e0dbf49086f
minwoorich <minwoorich@naver.com>
2024-10-18 19:50:05 +0900

    feat: [POST /auth/re-issue] 토큰 재발급 API 완성

--- src/main/java/site/javaghost/conolja/common/exception/ExceptionHandlerAdvice.java
+++ src/main/java/site/javaghost/conolja/common/exception/ExceptionHandlerAdvice.java
@@ -1,12 +1,15 @@
 package site.javaghost.conolja.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class ExceptionHandlerAdvice {
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler(Exception.class)
   public String handleInternalServerException(Exception e) {
     log.error("GlobalExceptionHandler : {}", e.getMessage());
--- src/main/java/site/javaghost/conolja/common/security/auth/AuthController.java
+++ src/main/java/site/javaghost/conolja/common/security/auth/AuthController.java
@@ -2,6 +2,7 @@ package site.javaghost.conolja.common.security.auth;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.javaghost.conolja.common.security.jwt.JwtProperties;
 import site.javaghost.conolja.common.security.jwt.JwtTokenDto;
 import site.javaghost.conolja.common.security.jwt.JwtTokenUtil;
 import site.javaghost.conolja.common.security.jwt.LoginRequest;
@@ -25,32 +27,34 @@ import java.util.Map;
 @Slf4j
 @Tag(name = "Auth", description = "인증 관련 API")
 public class AuthController {
-    private final AuthService authService;
-    private final JwtTokenUtil jwtTokenUtil;
+  private final AuthService authService;
+  private final JwtTokenUtil jwtTokenUtil;
+  private final JwtProperties props;
 
-    @PostMapping("/signup")
-    @Operation(summary = "회원가입", description = "회원가입을 합니다.")
-    public ResponseEntity<Map<String, String>> signup(
-      @Valid @RequestBody AccountCreateRequest request) {
-        authService.signup(request.toCommand());
-        return ResponseEntity.ok(Map.of("message", "회원가입 성공"));
-    }
+  @PostMapping("/signup")
+  @Operation(summary = "회원가입", description = "회원가입을 합니다.")
+  public ResponseEntity<Map<String, String>> signup(
+    @Valid @RequestBody AccountCreateRequest request) {
+    authService.signup(request.toCommand());
+    return ResponseEntity.ok(Map.of("message", "회원가입 성공"));
+  }
 
-    @PostMapping("/login")
-    @Operation(summary = "로그인", description = "로그인을 합니다.")
-    public ResponseEntity<JwtTokenDto> login(
-      @Valid @RequestBody LoginRequest loginRequest) {
+  @PostMapping("/login")
+  @Operation(summary = "로그인", description = "로그인을 합니다.")
+  public ResponseEntity<JwtTokenDto> login(
+    @Valid @RequestBody LoginRequest loginRequest) {
 
-        // LoginFilter 에서 인증 처리 완료
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        JwtTokenDto jwtTokenDto = jwtTokenUtil.generateToken(auth);
-        return ResponseEntity.ok(jwtTokenDto);
-    }
+    // LoginFilter 에서 인증 처리 완료
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    return ResponseEntity.ok(jwtTokenUtil.generateToken(auth));
+  }
 
-    @PostMapping("/re-issue")
-    @Operation(summary = "토큰 재발급", description = "토큰을 재발급 합니다.")
-    public ResponseEntity<Map<String, String>> issue(IssueRequest request) {
-
-        return ResponseEntity.ok(Map.of("message", "토큰 재발급 성공"));
-    }
+  @PostMapping("/re-issue")
+  @Operation(summary = "토큰 재발급", description = "리프래시 토큰을 통해 액세스 토큰을 재발급 합니다.")
+  public ResponseEntity<JwtTokenDto> reIssue(HttpServletRequest request) {
+    String headerValue = request.getHeader(props.header());
+    log.info("authorization: {}", headerValue);
+    String token = jwtTokenUtil.parseToken(headerValue);
+    return ResponseEntity.ok(jwtTokenUtil.reIssueToken(token));
+  }
 }
