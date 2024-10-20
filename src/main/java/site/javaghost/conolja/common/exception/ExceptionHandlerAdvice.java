1b8b0401accd47dcbf344dc5b3bfccf943cf5f88
Jhung Minwoo <66115670+minwoorich@users.noreply.github.com>
2024-10-20 15:36:09 +0900

    fix: 도커 이미지 빌드 및 배포 테스트 (#30)

--- src/main/java/site/javaghost/conolja/common/exception/ExceptionHandlerAdvice.java
+++ src/main/java/site/javaghost/conolja/common/exception/ExceptionHandlerAdvice.java
@@ -1,15 +1,19 @@
 package site.javaghost.conolja.common.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class ExceptionHandlerAdvice {
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler(Exception.class)
-  public String handleInternalServerException(Exception e) {
+  public CustomErrorResponse<?> handleInternalServerException(Exception e, HttpServletRequest request) {
     log.error("GlobalExceptionHandler : {}", e.getMessage());
-    return e.getMessage();
+    return CustomErrorResponse.create(request.getRequestURI().intern(), ErrorCode.INTERNAL_SERVER_ERROR);
   }
 }
--- src/main/java/site/javaghost/conolja/common/security/jwt/JwtTokenUtil.java
+++ src/main/java/site/javaghost/conolja/common/security/jwt/JwtTokenUtil.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import site.javaghost.conolja.common.exception.exceptions.JwtAuthenticationException;
+import site.javaghost.conolja.common.security.AccountRole;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
@@ -21,73 +22,97 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JwtTokenUtil {
 
-    private final JwtProperties jwtProperties;
+  private final JwtProperties jwtProperties;
 
-    public String parseToken(String headerValue) {
-        // 헤더가 비어있는지 확인
-        if (!StringUtils.hasText(headerValue)) {
-            throw JwtAuthenticationException.invalidAuthorizationHeader(headerValue);
-        }
-
-        final String prefixWithSpace = jwtProperties.prefix() + " ";
-
-        // 헤더가 Bearer 인지 아닌지 확인
-        if (!headerValue.startsWith(prefixWithSpace)) {
-            // 헤더 형식이 잘못된 경우 예외 처리
-            throw JwtAuthenticationException.invalidType(headerValue);
-        }
-        // prefix 이후의 토큰 부분 추출
-        return headerValue.substring(prefixWithSpace.length());
+  public String parseToken(String headerValue) {
+    // 헤더가 비어있는지 확인
+    if (!StringUtils.hasText(headerValue)) {
+      throw JwtAuthenticationException.invalidAuthorizationHeader(headerValue);
     }
 
-    // JWT 에서 Claim 추출
-    public String extractUsername(String accessToken) {
-        return getClaims(accessToken).getBody().getSubject();
+    final String prefixWithSpace = jwtProperties.prefix() + " ";
+
+    // 헤더가 Bearer 인지 아닌지 확인
+    if (!headerValue.startsWith(prefixWithSpace)) {
+      // 헤더 형식이 잘못된 경우 예외 처리
+      throw JwtAuthenticationException.invalidType(headerValue);
+    }
+    // prefix 이후의 토큰 부분 추출
+    return headerValue.substring(prefixWithSpace.length());
+  }
+
+  // JWT 에서 Claim 추출
+  public String extractUsername(String accessToken) {
+    return getClaims(accessToken).getBody().getSubject();
+  }
+
+  // JwtTokenDto 토큰 생성
+  public JwtTokenDto generateToken(Authentication authentication) {
+    // 사용자 정보 추출
+    String username = authentication.getName();
+    String authority = authentication.getAuthorities().stream().findFirst().get().getAuthority();
+
+    // 액세스 토큰 생성
+    String accessToken = generateAccessToken(username);
+
+    // 리프레시 토큰 생성
+    String refreshToken = generateRefreshToken();
+
+    return JwtTokenDto.by(accessToken, refreshToken, authority);
+  }
+
+  private String generateRefreshToken() {
+    Date date = new Date(System.currentTimeMillis() + jwtProperties.refresh().expiration().toMillis());
+    return Jwts.builder()
+      .setExpiration(date)
+      .signWith(getPrivateKey(jwtProperties.secret()), SignatureAlgorithm.HS512)
+      .compact();
+  }
+
+  private String generateAccessToken(String username) {
+    Date date = new Date(System.currentTimeMillis() + jwtProperties.expiration().toMillis());
+    log.info("expiration date : {}", date);
+    return Jwts.builder()
+      .setSubject(username)
+      .setIssuer(jwtProperties.issuer())
+      // 만료 시간 = 현재 시각 + 유효 기간
+      .setExpiration(date)
+      .setIssuedAt(new Date())
+      .signWith(getPrivateKey(jwtProperties.secret()), SignatureAlgorithm.HS512)
+      .compact();
+  }
+
+  public JwtTokenDto reIssueToken(String refreshToken) {
+    if (hasExpired(refreshToken)) {
+      throw JwtAuthenticationException.tokenExpired();
     }
 
-    // JwtTokenDto 토큰 생성
-    public JwtTokenDto generateToken(Authentication authentication) {
-        // 사용자 정보 추출
-        String username = authentication.getName();
-        String authority = authentication.getAuthorities().stream().findFirst().get().getAuthority();
+    String username = extractUsername(refreshToken);
+    String authority = AccountRole.USER.name();
 
-        // 액세스 토큰 생성
-        String accessToken = Jwts.builder()
-          .setSubject(username)
-          .setIssuer(jwtProperties.issuer())
-          // 만료 시간 = 현재 시각 + 유효 기간
-          .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.expiration().toMillis()))
-          .setIssuedAt(new Date())
-          .signWith(getPrivateKey(jwtProperties.secret()), SignatureAlgorithm.HS512)
-          .compact();
+    String accessToken = generateAccessToken(username);
 
-        // 리프레시 토큰 생성
-        String refreshToken = Jwts.builder()
-          .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.refresh().expiration().toMillis()))
-          .signWith(getPrivateKey(jwtProperties.secret()), SignatureAlgorithm.HS512)
-          .compact();
+    return JwtTokenDto.by(accessToken, refreshToken, authority);
+  }
 
-        return JwtTokenDto.by(accessToken, refreshToken, authority);
-    }
+  private SecretKeySpec getPrivateKey(String secretKey) {
+    byte[] keyBytes = Base64.getDecoder().decode(secretKey);
+    return new SecretKeySpec(keyBytes, SignatureAlgorithm.HS512.getJcaName());
+  }
 
-    private SecretKeySpec getPrivateKey(String secretKey) {
-        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
-        return new SecretKeySpec(keyBytes, SignatureAlgorithm.HS512.getJcaName());
-    }
+  public boolean hasExpired(String token) {
+    return hasExpired(getClaims(token));
+  }
 
-    public boolean hasExpired(String token) {
-        return hasExpired(getClaims(token));
-    }
+  private Jws<Claims> getClaims(String token) {
+    return Jwts.parserBuilder()
+      .setSigningKey(getPrivateKey(jwtProperties.secret()))
+      .build()
+      .parseClaimsJws(token);
+  }
 
-    private Jws<Claims> getClaims(String token) {
-        return Jwts.parserBuilder()
-          .setSigningKey(getPrivateKey(jwtProperties.secret()))
-          .build()
-          .parseClaimsJws(token);
-    }
-
-    private boolean hasExpired(Jws<Claims> claims) {
-        return !claims.getBody().getExpiration().before(new Date());
-    }
+  private boolean hasExpired(Jws<Claims> claims) {
+    return claims.getBody().getExpiration().before(new Date());
+  }
 }
 
