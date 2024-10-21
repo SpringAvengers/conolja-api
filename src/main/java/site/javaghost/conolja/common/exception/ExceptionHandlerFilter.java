e10b6f5d6358998599ce4f051bd868b9a044e40c
minwoorich <minwoorich@naver.com>
2024-10-21 18:35:10 +0900

    feat: 인증/인가 관련 예외처리 응답 작성

--- src/main/java/site/javaghost/conolja/common/exception/ExceptionHandlerFilter.java
+++ src/main/java/site/javaghost/conolja/common/exception/ExceptionHandlerFilter.java
@@ -1,10 +1,15 @@
 package site.javaghost.conolja.common.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 import site.javaghost.conolja.common.response.CustomErrorResponse;
 
@@ -12,22 +17,37 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+  private final ObjectMapper objectMapper;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
     try {
+      //  UnsupportedJwtException, MalformedJwtException, SignatureException, IllegalArgumentException;
       filterChain.doFilter(request, response);
     } catch (JwtAuthenticationException e) {
-      //토큰의 유효기간 만료
+      log.error("ExceptionHandlerFilter: ", e);
       setErrorResponse(response, request.getRequestURI(), e.getErrorCode());
+    } catch (UnsupportedJwtException e) {
+      log.error("ExceptionHandlerFilter: ", e);
+      setErrorResponse(response, request.getRequestURI(), ErrorCode.JWT_UNSUPPORTED_TOKEN);
+    } catch (MalformedJwtException e) {
+      log.error("ExceptionHandlerFilter: ", e);
+      setErrorResponse(response, request.getRequestURI(), ErrorCode.JWT_INVALID_TOKEN);
+    } catch (ExpiredJwtException e) {
+      log.error("ExceptionHandlerFilter: ", e);
+      setErrorResponse(response, request.getRequestURI(), ErrorCode.JWT_TOKEN_EXPIRED);
     } catch (Exception e) {
       // 서버 에러
+      log.error("ExceptionHandlerFilter: ", e);
       setErrorResponse(response, request.getRequestURI(), ErrorCode.INTERNAL_SERVER_ERROR);
     }
   }
 
   private void setErrorResponse(HttpServletResponse response, String path, ErrorCode errorCode) throws IOException {
-    ObjectMapper objectMapper = new ObjectMapper();
     response.setStatus(errorCode.getStatus());
     response.setCharacterEncoding("UTF-8");
     response.setContentType("application/json; charset=UTF-8");
