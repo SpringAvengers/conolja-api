d8c64f71532c2dd0915925175ccf6839b9dd0c36
minwoorich <minwoorich@naver.com>
2024-10-21 18:55:09 +0900

    feat: 요청 진입시 로그 찍는 인터셉터 추가

--- src/main/java/site/javaghost/conolja/common/security/jwt/LoginRequest.java
+++ src/main/java/site/javaghost/conolja/common/security/jwt/LoginRequest.java
@@ -1,5 +1,6 @@
 package site.javaghost.conolja.common.security.jwt;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
@@ -8,9 +9,10 @@ import lombok.Builder;
 @Builder
 @Valid
 public record LoginRequest(
-        @Email @Schema(description = "사용자명", example = "test@email.com")
-        String username,
-        @Schema(description = "사용자명", example = "1234")
-        String password
+  @Email @Schema(description = "사용자명", example = "test@email.com")
+  String username,
+  @Schema(description = "사용자명", example = "1234")
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)  // JSON 직렬화 시 마스킹 처리
+  String password
 ) {
 }
