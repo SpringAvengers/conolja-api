7d2d77763cb293d3c5e8c09f89715756c7e60430
Jhung Minwoo <66115670+minwoorich@users.noreply.github.com>
2024-10-22 18:06:36 +0900

    Feat setup (#32)
    * fixed untracked files
    
    * feat: [POST /auth/re-issue] 토큰 재발급 API 완성
    
    * chore: 로깅 코드 제거
    
    * feat: 500 에러에 대한 반환 타입 정의 (ExceptionHandlerAdvice)
    
    * fix: 도커 이미지 빌드 및 배포 테스트
    
    * chore: 안 쓰는 코드 제거
    
    * feat: 글로벌 예외처리기에 새로운 예외들 추가
    
    CustomLogicException 추가
    CustomErrorResponse 추가
    
    * feat: 글로벌 예외처리기에 JwtAuthenticationException 핸들러 추가
    
    * feat: ObjectMapper 사용시 LocalDateTime 지원 안하는 이슈 해결
    
    - Gradle 에 jackson 관련 모듈 추가
    - ObjectMapperConfig 추가
    
    * feat: 인증/인가 관련 예외처리 응답 작성
    
    * feat: 요청 진입시 로그 찍는 인터셉터 추가
    
    * feat: [POST /auth/re-issue] 토큰 재발급 API 완성
    
    * chore: 로깅 코드 제거
    
    * chore: 안 쓰는 코드 제거
    
    * feat: 글로벌 예외처리기에 새로운 예외들 추가
    
    CustomLogicException 추가
    CustomErrorResponse 추가
    
    * feat: 글로벌 예외처리기에 JwtAuthenticationException 핸들러 추가
    
    * feat: ObjectMapper 사용시 LocalDateTime 지원 안하는 이슈 해결
    
    - Gradle 에 jackson 관련 모듈 추가
    - ObjectMapperConfig 추가
    
    * feat: 인증/인가 관련 예외처리 응답 작성
    
    * feat: 요청 진입시 로그 찍는 인터셉터 추가
    
    * feat: 문자열 메세지만 응답하는 간단한 response 객체 생성 (SimpleResponse)
    
    * feat: @JwtToken 어노테이션 생성
    
    - 컨트롤러 메서드의 파라미터로 request 로 전달된 Jwt토큰을 추출 해낼 수 있다
    
    * fix: application-dev.yml 에 DB url 수정
    
    * fixed untracked files
    
    * fixed untracked files
    
    * fixed untracked files

--- src/main/java/site/javaghost/conolja/common/configs/WebConfig.java
+++ src/main/java/site/javaghost/conolja/common/configs/WebConfig.java
@@ -5,17 +5,32 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import site.javaghost.conolja.common.annotation.JwtToken;
+import site.javaghost.conolja.common.security.jwt.JwtTokenUtil;
+
+import java.util.List;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+  private final LogInterceptor logInterceptor;
+  private final ExtractJwtTokenArgumentResolver tokenArgumentResolver;
 
   @Bean
   public ObjectMapper objectMapper() {
@@ -27,9 +42,15 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(new LogInterceptor());
+    registry.addInterceptor(logInterceptor);
   }
 
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(tokenArgumentResolver);
+  }
+
+  @Component
   static class LogInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(
@@ -39,6 +60,27 @@ public class WebConfig implements WebMvcConfigurer {
     ) {
       log.info("[{}]{}", request.getMethod(), request.getRequestURI());
       return true;
+    }
+  }
+
+  @RequiredArgsConstructor
+  @Component
+  static class ExtractJwtTokenArgumentResolver implements HandlerMethodArgumentResolver {
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+      return parameter.hasParameterAnnotation(JwtToken.class);
+    }
+
+    @Override
+    public Object resolveArgument(@NonNull MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+      HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+      assert request != null;
+      return jwtTokenUtil.parseToken(request.getHeader("Authorization"));
     }
   }
 }
--- src/main/java/site/javaghost/conolja/common/security/auth/AuthController.java
+++ src/main/java/site/javaghost/conolja/common/security/auth/AuthController.java
@@ -1,8 +1,8 @@
 package site.javaghost.conolja.common.security.auth;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,13 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.javaghost.conolja.common.annotation.JwtToken;
+import site.javaghost.conolja.common.response.SimpleResponse;
 import site.javaghost.conolja.common.security.jwt.JwtProperties;
 import site.javaghost.conolja.common.security.jwt.JwtTokenDto;
 import site.javaghost.conolja.common.security.jwt.JwtTokenUtil;
 import site.javaghost.conolja.common.security.jwt.LoginRequest;
 import site.javaghost.conolja.domains.account.presentation.dto.AccountCreateRequest;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,10 +33,10 @@ public class AuthController {
 
   @PostMapping("/signup")
   @Operation(summary = "회원가입", description = "회원가입을 합니다.")
-  public ResponseEntity<Map<String, String>> signup(
+  public ResponseEntity<SimpleResponse> signup(
     @Valid @RequestBody AccountCreateRequest request) {
     authService.signup(request.toCommand());
-    return ResponseEntity.ok(Map.of("message", "회원가입 성공"));
+    return ResponseEntity.ok(SimpleResponse.of("회원가입 성공"));
   }
 
   @PostMapping("/login")
@@ -51,9 +51,7 @@ public class AuthController {
 
   @PostMapping("/re-issue")
   @Operation(summary = "토큰 재발급", description = "리프래시 토큰을 통해 액세스 토큰을 재발급 합니다.")
-  public ResponseEntity<JwtTokenDto> reIssue(HttpServletRequest request) {
-    String headerValue = request.getHeader(props.header());
-    String token = jwtTokenUtil.parseToken(headerValue);
+  public ResponseEntity<JwtTokenDto> reIssue(@Parameter(hidden = true) @JwtToken String token) {
     return ResponseEntity.ok(jwtTokenUtil.reIssueToken(token));
   }
 }
